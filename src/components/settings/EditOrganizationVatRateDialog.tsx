90acbe5e0274b2379d4c80913a4ba2a166eb5c4e
MorganeLecurieux <morgane.lecurieux.lafayette@gmail.com>
2023-03-23 11:01:15 -0500

    vatrate orga

--- src/components/settings/EditOrganizationVatRateDialog.tsx
+++ src/components/settings/EditOrganizationVatRateDialog.tsx
@@ -1,14 +1,16 @@
-import { forwardRef, useState, useEffect } from 'react'
+import { forwardRef, RefObject } from 'react'
 import { gql } from '@apollo/client'
+import { useFormik } from 'formik'
 import styled from 'styled-components'
+import { object, number } from 'yup'
 import { InputAdornment } from '@mui/material'
 
 import { Dialog, Button, DialogRef } from '~/components/designSystem'
-import { TextInput } from '~/components/form'
+import { TextInputField } from '~/components/form'
 import { useInternationalization } from '~/hooks/core/useInternationalization'
 import { useUpdateVatRateOrganizationMutation, LagoApiError } from '~/generated/graphql'
 import { theme } from '~/styles'
-import { addToast, hasDefinedGQLError } from '~/core/apolloClient'
+import { addToast } from '~/core/apolloClient'
 
 gql`
   mutation updateVatRateOrganization($input: UpdateOrganizationInput!) {
@@ -32,15 +34,33 @@ export const EditOrganizationVatRateDialog = forwardRef<
   EditOrganizationVatRateDialogProps
 >(({ vatRate }: EditOrganizationVatRateDialogProps, ref) => {
   const { translate } = useInternationalization()
-  const [mutationError, setMutationError] = useState<string | undefined>(undefined)
-  const [localVatRate, setLocalVatRate] = useState<number>(vatRate)
   const [updateVatRate] = useUpdateVatRateOrganizationMutation({
     context: { silentErrorCodes: [LagoApiError.UnprocessableEntity] },
+    onCompleted() {
+      addToast({
+        message: translate('text_62728ff857d47b013204c86f'),
+        severity: 'success',
+      })
+      ;(ref as unknown as RefObject<DialogRef>)?.current?.closeDialog()
+    },
   })
-
-  useEffect(() => {
-    setLocalVatRate(vatRate)
-  }, [vatRate])
+  const formikProps = useFormik<{ vatRate?: number }>({
+    initialValues: {
+      vatRate: vatRate ?? undefined,
+    },
+    validationSchema: object().shape({
+      vatRate: number()
+        .min(0, 'text_6272a16eea94bd01089abaa7')
+        .max(100, 'text_6272a16eea94bd01089abaa7')
+        .required('text_6272a16eea94bd01089abaa7'),
+    }),
+    enableReinitialize: true,
+    onSubmit: async (values) => {
+      await updateVatRate({
+        variables: { input: { billingConfiguration: { vatRate: Number(values.vatRate) } } },
+      })
+    },
+  })
 
   return (
     <Dialog
@@ -53,31 +73,17 @@ export const EditOrganizationVatRateDialog = forwardRef<
             variant="quaternary"
             onClick={() => {
               closeDialog()
-              setLocalVatRate(vatRate)
+              formikProps.resetForm()
             }}
           >
             {translate('text_62728ff857d47b013204c7e4')}
           </Button>
           <Button
             variant="primary"
-            disabled={
-              typeof localVatRate !== 'number' || localVatRate === vatRate || !!mutationError
-            }
+            // @ts-ignore
+            disabled={formikProps.values.vatRate === ''}
             onClick={async () => {
-              const res = await updateVatRate({
-                variables: { input: { billingConfiguration: { vatRate: localVatRate } } },
-              })
-              const { errors } = res
-
-              if (hasDefinedGQLError('ValueIsOutOfRange', errors)) {
-                setMutationError(translate('text_6272a16eea94bd01089abaa7'))
-              } else if (!errors) {
-                addToast({
-                  message: translate('text_62728ff857d47b013204c86f'),
-                  severity: 'success',
-                })
-                closeDialog()
-              }
+              formikProps.handleSubmit()
             }}
           >
             {translate('text_62728ff857d47b013204c7fa')}
@@ -86,15 +92,17 @@ export const EditOrganizationVatRateDialog = forwardRef<
       )}
     >
       <Content>
-        <TextInput
+        <TextInputField
+          name="vatRate"
+          formikProps={formikProps}
           label={translate('text_62728ff857d47b013204c7a2')}
-          value={localVatRate}
-          error={mutationError}
+          // value={localVatRate}
+          // error={mutationError}
           beforeChangeFormatter="positiveNumber"
-          onChange={(value) => {
-            !!mutationError && setMutationError(undefined)
-            setLocalVatRate(Number(value))
-          }}
+          // onChange={(value) => {
+          //   !!mutationError && setMutationError(undefined)
+          //   setLocalVatRate(Number(value))
+          // }}
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
